d12010e426a54698bbddeb6085c1496e2860e97f
Hir0Yat0 <xToppyThailandx@gmail.com>
2023-12-11 22:14:10 +0700

    added some tests to main.v

--- src/main.v
+++ src/main.v
@@ -1,10 +1,29 @@
 
-module main (
+module main /*(
     // ports
 
-    
 
-);
+);*/
+    reg [7:0] alu_result;
+    reg [3:0] alu_component_select;
+    reg alu_output_select;
+    reg [7:0] alu_input_1, alu_input_2;
+
+    alu the_alu(
+        .output_1 alu_result,
+        .alu_component_select alu_component_select
+        .input_1 alu_input_1,
+        .input_2 alu_input_2,
+    );
     
+    initial begin
+        alu_component_select = 3'b000;
+        alu_input_1 = 8'b00000000; 
+        alu_input_2 = 8'b00000001; #10
+        $display("IN1:[%b] IN2:[%b] OUT1:[%b]",alu_input_1,alu_input_2,alu_result);
+        #10;
+        
+    end
+
 endmodule
 
