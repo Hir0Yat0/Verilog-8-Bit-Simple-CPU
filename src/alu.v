a15d307a062d3b1dacbe75fa8cd46c55306242c1
Hir0Yat0 <xToppyThailandx@gmail.com>
2023-12-11 21:57:19 +0700

    added more things to alu.v

--- src/alu.v
+++ src/alu.v
@@ -32,16 +32,38 @@ module alu (
     always @(*) begin
         
         case (alu_component_select)
-            3'b000: adder add(
-                .output_1 result,
-                .input_1 input_1,
-                .input_2 input_2 
+            3'b000: 
+                adder add(
+                    .output_1 result,
+                    .input_1 input_1,
+                    .input_2 input_2 
                 ); 
-            3'b001: multer mult(
-                .output_1 result,
-                .input_1 input_1,
-                .input_2 input_2
+            3'b001:
+                multer mult(
+                    .output_1 result,
+                    .input_1 input_1,
+                    .input_2 input_2
                 );
+            3'b010:
+                and_gate and(
+                    .out result,
+                    .a input_1,
+                    .b input_2,
+                ) ;
+            3'b011:
+                or_gate or(
+                    .out result,
+                    .a input_1,
+                    .b input_2,
+                );
+            3'b100:
+                not_gate not(
+                    .out result,
+                    .in input_1,
+                );
+            // 3'b101:
+            // 3'b110:
+            // 3'b111:
             default: 
                 result = input_1;
         endcase
