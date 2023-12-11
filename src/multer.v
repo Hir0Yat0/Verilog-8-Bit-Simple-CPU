c8d6c877ec22ed3eb8bdfd4d6edf3969b366546a
Hir0Yat0 <xToppyThailandx@gmail.com>
2023-12-11 21:25:39 +0700

    added multer.v

---
+++ src/multer.v
@@ -0,0 +1,19 @@
+
+module multer (
+    // ports
+
+    input_1,
+    input_2,
+    output_1
+
+);
+
+    // 8 bits inputs outputs
+    input [7:0] input_1, input_2; //[7:0]
+    output [7:0] output_1
+    /* copy paste from adder.v, changed + to * */
+    always @(*) begin
+        assign output_1 = input_1 * input_2;
+    end
+    
+endmodule
